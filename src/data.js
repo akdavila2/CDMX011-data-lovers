9e444a384666971f16a566a5216243c6908a3582
akdavila2 <anakarina.daviladavila@gmail.com>
2021-07-27 09:58:36 -0500

    ajustes a funcion sort data sin console.log

--- src/data.js
+++ src/data.js
@@ -13,27 +13,21 @@ export function DataManager() {
             this.films = films;
             this.years = films.map((item) => item.release_date).filter((item) => ![undefined].includes(item));
             this.years = [...new Set(this.years)]; //Un valor en un Set sólo puede estar una vez, el operador de descanso: ... lo que hace es recorrer los elementos de un objeto iterable y devolverlos separados por coma.
-            console.log('los años ', this.years);
             this.producer = films.map((item) => item.producer).filter((item) => ![undefined].includes(item));
             this.producer = [...new Set(this.producer)];
-            console.log('los productores', this.producer);
             this.title = films.map((item) => item.title);
-            console.log('somos los titulos', this.title);
         }
         //Metodo para cargar la data.
     this.load = async() => { //funcion es asincrona
         const response = await fetch('/data/ghibli/ghibli.json'); //El método fetch() es una peticion get o post a una url en nuestro caso es local.
         this.data = await response.json(); // respuesta de la funcion asincrona, por medio de la palabra await
-        console.log("data obtenida", this.data);
         this.ready = true;
         process();
         return this.ready;
     }
     this.filterByProducer = (producer) => { //Metodo filtrar por productor.
-            console.log('sirve por fa ', this.films);
             if (!this.films) return []; //no se ha ejecutado el metodo load, no hay films para cargar.
             return this.films.filter((item) => {
-                console.log("comparamos", `${item.producer} == ${producer}`)
                 return item.producer == producer
             });
         }
@@ -44,9 +38,7 @@ export function DataManager() {
         if (!types.includes(option)) return new Error('El tipo no existe');
         return this.films.sort((filmA, filmB) => {
             if (filmA[field] === filmB[field]) return 0; //aqui comparo los campos
-            console.log('soy campo a=>', filmA[field], 'soy campo b=>', filmB[field], 'soy campo solo=>', field);
             let isUp = checkIsUp(filmA[field], filmB[field]);
-            console.log('que imprime isUp', isUp)
             if (option === 'upward') return isUp ? 1 : -1;
             if (option === 'falling') return isUp ? -1 : 1;
         });
--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import { DataManager, } from './data.js';
 let manager = new DataManager; /// se instancia todo lo que esta adentro del DataManager, todos los this, aqui podre verlos.
 console.log('la clase', manager);
-console.log('que trae producer', manager.filterByProducer('Hayao Miyazaki'));
-console.log('que trae años', manager.filterByProducer(1988));
-
 const templatePoster = (manager) => {
     return `<picture>
-   
-    <img src= "${manager.poster}" alt= "${manager.title}" class= "imgfilm" id= "${manager.id}">
-
+       <img src= "${manager.poster}" alt= "${manager.title}" class= "imgfilm" id= "${manager.id}">
     </picture>`;
 }
 const createOpcion = (value, text) => {
@@ -19,14 +14,12 @@ const createOpcion = (value, text) => {
     }
     //constantes del DOM
 const selectProducer = document.querySelector('#selectProducer');
-console.log('que me tares selectProducer', selectProducer)
 const gridMovies = document.querySelector('#gridMovies');
 //const orderDate = document.querySelector('#orderDate');
 //const orderFilms = document.querySelector('#orderFilms');
 const reset = document.querySelector('#reset')
 const start = async() => {
     await manager.load();
-    console.log('el manager', manager);
     //Mostrando los poster
     let outputPoster = [];
     const div = document.createElement('div'); //createElement() crea un elemento HTML especificado por su tagName.
@@ -46,17 +39,14 @@ const start = async() => {
     const mySelects = (select) => {
         select.addEventListener('change', event => {
             let field = select.dataset.field;
-            console.log('soy field', field);
             let option = select.value;
             let data = manager.sortData(option, field);
-            console.log('VAriable data', data);
             let listByOrder = '';
             data.forEach((item) => listByOrder += templatePoster(item));
             div.innerHTML = listByOrder;
         })
     }; // esto se ejecuta por cada select.
-    let quiensoy = document.querySelectorAll(`[data-field]`).forEach(mySelects);
-    console.log('quien soy', quiensoy)
+    document.querySelectorAll(`[data-field]`).forEach(mySelects);
 
     const restore = () => document.getElementById('myForm').reset; //Restaurando los valores con el metodo HTMLFormElement.reset
 
